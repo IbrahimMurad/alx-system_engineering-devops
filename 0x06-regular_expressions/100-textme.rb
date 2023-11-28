7f4a69b466e1449b7f28de5881d078163b1fa9f6
IbrahimMurad <Ibrahimmorad31@gmail.com>
2023-11-28 19:43:48 +0200

    from, to, flags

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,2 +1,2 @@
 #!/usr/bin/env ruby
-puts ARGV[0].scan(/((?<=\[from:)+?\w*(?=\]))|((?<=\[to:)\+?\w*(?=\]))|((?<=\[flags:)[-10:]*(?=\]))/).join
+puts ARGV[0].scan(/((?<=\[from:)+?\w*(?=\]))|((?<=\[to:)\+?\w*(?=\]))|((?<=\[flags:)[-10:]*(?=\]))/).flatten.compact.join(", ")
