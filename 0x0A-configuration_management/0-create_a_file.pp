1e462576e1268bea233eec457b1d52753962b63f
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-02-23 14:11:40 +0200

    school=I love puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -6,8 +6,8 @@
 # File contains I love Puppet
 
 file { '/tmp/school' :
-mode     => '0744',
-owner    => 'www-data',
-group    => 'www-data',
-content  => 'I love Puppet'
+  mode    => '0744',
+  owner   => 'www-data',
+  group   => 'www-data',
+  content => 'I love Puppet'
 }
