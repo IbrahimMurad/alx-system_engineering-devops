641d974039ed41eed54667b4a8c57c2864666224
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-02-23 16:55:30 +0200

    Flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -8,8 +8,12 @@ package { 'python3-pip':
   ensure => installed,
 }
 
-package { 'Flask':
+package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
   require  => Package['python3-pip'],
+}
+
+package { 'Werkzeug':
+  ensure => '2.1.1',
 }
