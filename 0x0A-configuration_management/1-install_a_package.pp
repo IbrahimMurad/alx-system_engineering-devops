ddc00148f6e4a624902b9556a7d4682af48ada25
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-02-23 16:54:07 +0200

    Flask

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,15 @@
 # install flask version 2.1.0 from pip3
 
+package { 'python3.8':
+  ensure => '3.8.10',
+}
+
+package { 'python3-pip':
+  ensure => installed,
+}
+
 package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
+  require  => Package['python3-pip'],
 }
