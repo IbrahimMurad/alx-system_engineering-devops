87e00de38583b6d069fb2818717775bf727dce65
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-02-23 16:18:19 +0200

    2-execute_a_command.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,7 @@
 # install flask version 2.1.0 from pip3.
 
-package { 'flask':
+package { 'Flask':
+  name            => 'flask'
   ensure          => '2.1.0',
   provider        => 'pip3',
   install_options => ['--user'],
