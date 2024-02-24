fffcd24a713a9067d5bf1c80385342503d46e26a
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-02-24 20:18:27 +0200

    puppet ssh_config

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,16 @@
+# set up client SSH configuration file so that he can connect to a server without typing a password.
+
+file_line { 'ssh config':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '^\#\ \ \ \ IdentityFile',
+  match  => '^\#\ \ \ \ IdentityFile\ \~/\.ssh/school',
+}
+
+
+file_line { 'ssh config':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  line   => '^\#\ \ \ PasswordAuthentication',
+  match  => '^\#\ \ \ PasswordAuthentication\ no',
+}
