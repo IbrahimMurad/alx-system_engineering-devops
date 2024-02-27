75cdcf8187960112c623061e309b53c8406180df
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-02-27 21:33:42 +0200

    7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -19,28 +19,3 @@ exec { 'listen to 80':
   command => '/usr/sbin/ufw allow \'Nginx HTTP\'',
   require => Package['ufw', 'nginx'],
 }
-
-file { 'main page':
-  ensure  => present,
-  path    => '/var/www/html/index.nginx-debian.html',
-  require => Package['nginx'],
-  content => 'Hello World!"\n"',
-}
-file_line { 'redirect_me':
-  ensure => present,
-  path   => '/etc/nginx/sites-enabled/default',
-  match  => '^\nserver_name _;',
-  line   => "	server_name _;
-
-	location /redirect_me {
-
-		return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-
-	}
-",
-}
-service { 'nginx':
-  ensure    => running,
-  enable    => true,
-  subscribe => File['main page', 'redirect_me'],
-}
