a952f2b4b9a899876984b1f51e9417a29f38d4cd
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-02-27 17:18:42 +0200

    7-puppet_install_nginx_web_server.pp

---
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -0,0 +1,31 @@
+# automatically configure an Ubuntu machine to respect the following requirements
+#   Nginx should be listening on port 80
+#   When querying Nginx at its root / with a GET request (requesting a page) using curl, it must return a page that contains the string Hello World!
+#   The redirection must be a “301 Moved Permanently”
+
+
+package { 'nginx'
+  ensure   => latest,
+  provider => 'apt-get',
+}
+
+package { 'ufw'
+  ensure => latest,
+}
+
+exec { 'ufw allow "Nginx HTTP"'
+  require => Package['ufw'],
+}
+
+file { '/var/www/html/index.nginx-debian.html'
+  ensure  => present,
+  require => Package['nginx'],
+  content => 'Hello World!'
+}
+
+file_line { 'redirect_me':
+  ensure => present,
+  path   => '/etc/nginx/sites-enabled/default',
+  line   => '^\tserver_name _;',
+  match  => '\tserver_name _;\n\n\tlocation /redirect_me \{\n\n\t\treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\n\n\t\}\n",
+}
