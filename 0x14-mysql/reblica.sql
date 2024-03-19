7e19ada0d1597e4cb619a335c9ff5c49d496754f
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-03-19 17:57:28 +0200

    Prerequisites

---
+++ 0x14-mysql/reblica.sql
@@ -0,0 +1,16 @@
+CREATE USER 'holberton_user'@localhost IDENTIFIED BY 'projectcorrection280hbtn';
+GRANT REPLICATION CLIENT ON *.* TO 'holberton_user'@'localhost';
+FLUSH PRIVILEGES;
+
+
+CREATE DATABASE IF NOT EXISTS tyrell_corp;
+USE tyrell_corp;
+CREATE TABLE IF NOT EXISTS nexus6 (id int,name varchar(64));
+INSERT INTO nexus6 (id, name) VALUES (1, 'Leon');
+GRANT SELECT ON tyrell_corp.nexus6 TO 'holberton_user'@localhost;
+FLUSH PRIVILEGES;
+
+
+CREATE USER 'replica_user'@'%' IDENTIFIED BY 'replica_pass';
+GRANT REPLICATION SLAVE ON *.* TO 'replica_user'@'%';
+GRANT SELECT ON mysql.user TO 'holberton_user'@localhost;
