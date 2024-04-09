4874d7ff6a8383bd5e2eb09542f2640076efa09c
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-04-10 00:51:58 +0200

    all hot

---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""This module defines a functions that
+fetches the number of subscribers for a subreddit.
+"""
+
+import requests
+
+
+def recurse(subreddit, hot_list=[]):
+    """ returns a list containing the titles of all hot
+    articles for a given subreddit """
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    header = {"User-Agent": "Mozilla/5.0"}
+    para = {'after': hot_list[-1]} if hot_list else {}
+    response = requests.get(
+        url,
+        headers=header,
+        params=para,
+        allow_redirects=False
+        )
+    if response.status_code == 200:
+        data = response.json()['data']
+        hot_list.append(data['after'])
+        title_arr = [post['data']['title'] for post in data['children']]
+        return title_arr.append(recurse(subreddit, hot_list))
+    else:
+        if hot_list:
+            return []
+        else:
+            return None
