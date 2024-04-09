64bf4bef5bd5560953ebaacc46c96c0132e0ec70
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-04-09 21:22:19 +0200

    subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ def number_of_subscribers(subreddit):
     """ returns the number of subscribers for subreddit """
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     header = {'User-Agent': 'MyRedditApp/1.0 (by /u/Aggravating_Page8361)'}
-    with requests.get(url, headers=header) as response:
+    with requests.get(url, headers=header, allow_redirects=False) as response:
         if response.status_code == 200:
             data = response.json()['data']
             return data['subscribers']
