a2cc5f4d22fc560fa304e4aa3bac5237522105bd
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-04-09 21:44:55 +0200

    fix

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,7 +9,7 @@ import requests
 def number_of_subscribers(subreddit):
     """ returns the number of subscribers for subreddit """
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    header = {'User-Agent': 'Mozilla/5.0'}
+    header = {"User-Agent": "Mozilla/5.0"}
     response = requests.get(url, headers=header, allow_redirects=False)
     if response.status_code == 200:
         data = response.json()['data']
