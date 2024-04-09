963fc07f847c54a2d0ae0eeec0605256cf29b2e1
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-04-09 21:25:53 +0200

    subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,6 +5,7 @@ fetches the number of subscribers for a subreddit.
 
 import requests
 
+
 def number_of_subscribers(subreddit):
     """ returns the number of subscribers for subreddit """
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
