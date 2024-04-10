a1600163144285d95bc45e23eb035f98e1deca9f
IbrahimMurad <Ibrahimmorad31@gmail.com>
2024-04-10 17:53:45 +0200

    some fixes

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,17 +9,20 @@ import requests
 def top_ten(subreddit):
     """ returns the number of subscribers for subreddit """
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
-    header = {"User-Agent": "Mozilla/5.0"}
+    header = {"User-Agent": "Mozilla/5.0", "Accept": "application/json"}
     para = {'limit': 10}
-    response = requests.get(
+    try:
+        response = requests.get(
         url,
         headers=header,
         params=para,
         allow_redirects=False
         )
-    if response.status_code == 200:
-        data = response.json()['data']['children']
-        for post in data:
-            print(post['data']['title'])
-    else:
+        if response.status_code == 200:
+            data = response.json()['data']['children']
+            for post in data:
+                print(post.get('data').get('title'))
+        else:
+            print('None')
+    except Exception:
         print('None')
